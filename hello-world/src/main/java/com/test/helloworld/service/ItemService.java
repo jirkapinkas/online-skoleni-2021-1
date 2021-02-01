52ef43a8732789340024ae037f4ac113cf33f9b9
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-02-01 12:50:02 +0100

    + lombok & constructor dependency injection

--- hello-world/src/main/java/com/test/helloworld/service/ItemService.java
+++ hello-world/src/main/java/com/test/helloworld/service/ItemService.java
@@ -1,17 +1,31 @@
 package com.test.helloworld.service;
 
 import com.test.helloworld.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+//@RequiredArgsConstructor
 public class ItemService {
 
-    @Autowired
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
+
+//    @Value("${java.version}")
+    private String javaVersion;
+
+    public ItemService(ItemRepository itemRepository, @Value("${java.version}") String javaVersion) {
+        this.itemRepository = itemRepository;
+        this.javaVersion = javaVersion;
+    }
+
+//    public ItemService(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
 
     public String getItemCount() {
-        return "Item count is: " + itemRepository.count();
+        return "Item count is: " + itemRepository.count() + " & Java version is: " + javaVersion;
     }
 
 }
