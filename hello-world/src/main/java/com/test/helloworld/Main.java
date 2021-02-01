efa3abfb863dd1b093c10b1fa1f2200a50ad28f5
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-02-01 13:36:10 +0100

    + pripojeni do databaze pomoci HikariCP

--- hello-world/src/main/java/com/test/helloworld/Main.java
+++ hello-world/src/main/java/com/test/helloworld/Main.java
@@ -1,13 +1,26 @@
 package com.test.helloworld;
 
 import com.test.helloworld.service.ItemService;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan // skenuje aktualni balicek ("com.test.helloworld") a vsechny jeho podbalicky!!!
 public class Main {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:hsqldb:hsql://localhost/eshop");
+        ds.setUsername("sa");
+        ds.setPassword("");
+        return ds;
+    }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext
