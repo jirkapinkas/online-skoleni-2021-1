a2fe8eeb8af0a7611bd48636205d63683e0e2881
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-02-01 14:31:14 +0100

    Vysvetleni @Bean a volani dataSource() metody

--- hello-world/src/main/java/com/test/helloworld/Main.java
+++ hello-world/src/main/java/com/test/helloworld/Main.java
@@ -2,6 +2,7 @@ package com.test.helloworld;
 
 import com.test.helloworld.service.ItemService;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,6 +17,7 @@ public class Main {
 
     @Bean
     public DataSource dataSource() {
+        System.out.println("dataSource constructed!!!");
         HikariDataSource ds = new HikariDataSource();
         ds.setJdbcUrl("jdbc:hsqldb:hsql://localhost/eshop");
         ds.setUsername("sa");
@@ -23,10 +25,18 @@ public class Main {
         return ds;
     }
 
+//    @Bean
+//    public JdbcTemplate jdbcTemplate() {
+//        System.out.println("jdbcTemplate constructed!!!");
+//        return new JdbcTemplate(dataSource()); // POZOR! TOHLE FUNGUJE POUZE VE TRIDACH S ANOTACI @Configuration !!!
+//    }
+
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource()); // TODO VRATIT SE SEM!!!
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        System.out.println("jdbcTemplate constructed!!!");
+        return new JdbcTemplate(dataSource);
     }
+
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext
