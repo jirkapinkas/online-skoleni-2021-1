edb9ffe46481ee6d62f7ea5f164863d47271d6da
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-02-02 11:29:09 +0100

    Cute dog hello world

--- eshop-web/src/main/java/com/test/eshopweb/controller/HelloController.java
+++ eshop-web/src/main/java/com/test/eshopweb/controller/HelloController.java
@@ -1,8 +1,13 @@
 package com.test.eshopweb.controller;
 
 import com.test.eshopweb.pojo.Message;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 public class HelloController {
@@ -11,6 +16,12 @@ public class HelloController {
     @GetMapping("/hello")
     public Message message() {
         return new Message("Spring Boot Works! :-) ^_^");
+    }
+
+    // http://localhost:8080/dog
+    @GetMapping(path = "/dog", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] dog() throws IOException {
+        return Files.readAllBytes(Paths.get("files", "dog.jpeg"));
     }
 
 }
