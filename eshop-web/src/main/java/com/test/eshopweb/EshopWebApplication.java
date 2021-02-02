3e2416aea97d2581e7ab9d2cf94211c162b303e7
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-02-02 11:14:05 +0100

    Co v sobe skryva @SpringBootApplication

--- eshop-web/src/main/java/com/test/eshopweb/EshopWebApplication.java
+++ eshop-web/src/main/java/com/test/eshopweb/EshopWebApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @SpringBootApplication v sobe schovava:
+// @Configuration
+// @ComponentScan
+// @EnableAutoConfiguration
 public class EshopWebApplication {
 
     public static void main(String[] args) {
