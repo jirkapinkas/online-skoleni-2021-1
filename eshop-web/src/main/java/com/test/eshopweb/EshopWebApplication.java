6eb398bfe03ec9fbb3b234aa03382a24e44cc7df
Jirka Pinkas <jirka.pinkas@gmail.com>
2021-02-02 10:57:18 +0100

    spring boot app hello world

---
+++ eshop-web/src/main/java/com/test/eshopweb/EshopWebApplication.java
@@ -0,0 +1,13 @@
+package com.test.eshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopWebApplication.class, args);
+    }
+
+}
